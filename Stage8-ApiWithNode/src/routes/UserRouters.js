bf2b071c264a9d1eba385afaa001207d28fe8fe3
César Rieg <cesarjeanrieg97@gmail.com>
2023-09-21 22:36:47 -0300

    Adicionando middleware para controle de fluxo da aplicação

--- Stage8-ApiWithNode/src/routes/UserRouters.js
+++ Stage8-ApiWithNode/src/routes/UserRouters.js
@@ -5,13 +5,30 @@ const userRoutes = Router();
 const userController = new UserController();
 
 
+
+function middlewareApplication(request, response, next){
+    console.log("Você passou pelo Middlleware");
+
+    if (!request.body.isAdmin){
+        return response.status(401).json({ message: "User unauthorized." });
+    }
+
+    next();
+}
+
+
+
+
+
+
+
 // Get - Requisitar por query params
 userRoutes.get("/", (request, response) => {
     const { page, limit } = request.query;
     response.send(`Página: ${page}. Quantidade de registros por página: ${limit}.`);
 });
 
-userRoutes.post("/", (request, response) => {
+userRoutes.post("/", middlewareApplication, (request, response) => {
     userController.Create(request, response);
 });
 
