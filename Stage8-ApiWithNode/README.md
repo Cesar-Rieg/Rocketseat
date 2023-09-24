8276c69cd476729dc37d50e65ac290c58e01524c
Cesar-Rieg <cesarjeanrieg97@gmail.com>
2023-09-24 10:20:45 -0300

    Instalando pacotes Sqlite e configurando Banco de Dados

--- Stage8-ApiWithNode/README.md
+++ Stage8-ApiWithNode/README.md
@@ -5,6 +5,7 @@ Para criar o projeto:
 3 - "npm install express --save" => instalar dependência/biblioteca express ao projeto (lib para cuidar das requisições e respostas)
 4 - "npm install nodemon --save-dev" => instalar biblioteca servidor (-dev comando para instalar apenas no modo desenvolvimento)
 5 - "npm install express-async-errors --save"
+6 - "npm install sqlite3 sqlite --save" => instalar pacote do banco de dados
 
 Após clonar, deve se executar:
 
--- Stage8-ApiWithNode/src/server.js
+++ Stage8-ApiWithNode/src/server.js
@@ -1,21 +1,24 @@
 const PORT = 3333;
 
 require("express-async-errors");
+const DataBase = require("./database/sqlite");
 const ApplicationError = require("./utils/ApplicationError.js");
 const express = require("express");
 const routes = require("./routes/index.js");
 
 const app = express();
 // Necessário para api receber os dados em formato JSON por POST
-app.use(express.json()); 
+app.use(express.json());
 app.use(routes);
+
+DataBase();
 
 app.use((error, request, response, next) => {
     if(error instanceof ApplicationError){
         return response.status(error.StatusCode).json( {
             Status: "Error",
             StatusCode: error.StatusCode,
-            Message: error.Message            
+            Message: error.Message
         })
     }
 
@@ -44,4 +47,3 @@ app.get("/message/:id/:user", (request, response) => {
     const { id, user } = request.params;
     response.send(`MessageId: ${id}. User: ${user}.`);
 });
-
