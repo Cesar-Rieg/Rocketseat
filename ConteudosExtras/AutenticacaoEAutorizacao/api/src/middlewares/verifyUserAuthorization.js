e326b9b1c4205d2d7adbf602cc56c379ce2b2638
César Rieg <cesarjeanrieg97@gmail.com>
2023-10-23 23:50:20 -0300

    criando middleware de autorização

---
+++ ConteudosExtras/AutenticacaoEAutorizacao/api/src/middlewares/verifyUserAuthorization.js
@@ -0,0 +1,15 @@
+const AppError = require("../utils/AppError");
+
+function verifyUserAuthorization(userRole) {
+    return (request, response, next) => {
+        const { role } = request.user;
+
+        if (role !== userRole) {
+            throw new AppError("Unauthorized request from this user.", 401);
+        }
+
+        return next();
+    }
+}
+
+module.exports = verifyUserAuthorization;
