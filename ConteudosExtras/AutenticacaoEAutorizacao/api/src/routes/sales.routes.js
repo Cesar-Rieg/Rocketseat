ad0cc73e50901fd1bd9315e74294b64892a5e9c2
César Rieg <cesarjeanrieg97@gmail.com>
2023-10-24 00:00:20 -0300

    aplicando middleware no sales.routes

--- ConteudosExtras/AutenticacaoEAutorizacao/api/src/routes/sales.routes.js
+++ ConteudosExtras/AutenticacaoEAutorizacao/api/src/routes/sales.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const SalesController = require("../controllers/SalesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization.js");
 
 const salesRoutes = Router();
 
 const salesController = new SalesController();
 
 salesRoutes.use(ensureAuthenticated);
+salesRoutes.use(verifyUserAuthorization("admin"));
 
 salesRoutes.get("/", salesController.index);
 
